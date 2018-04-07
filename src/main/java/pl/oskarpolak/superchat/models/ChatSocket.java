09d16f939a15b9b825ba6af6ab30f1785f76d4f7
Oskar Polak <biuro@fivarto.pl>
2018-04-07 13:45:52 +0200

    kkk

--- src/main/java/pl/oskarpolak/superchat/models/ChatSocket.java
+++ src/main/java/pl/oskarpolak/superchat/models/ChatSocket.java
@@ -1,5 +1,6 @@
 package pl.oskarpolak.superchat.models;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -30,6 +31,10 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         userList.add(new UserChatModel(session));
+
+        UserChatModel userChatModel = findUserBySessionId(session);
+        userChatModel.sendMessage("Witaj na naszym chacie!");
+        userChatModel.sendMessage("Twoja pierwsza wiadomość będzie Twoim nickiem");
     }
 
     @Override
@@ -39,12 +44,20 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        UserChatModel userChatModel = findUserBySessionId(session);
+
+        if(userChatModel.getNickname() == null){
+            userChatModel.setNickname(message.getPayload());
+            userChatModel.sendMessage("Ustawiono Twój nick!");
+            return;
+        }
+
         sendMessageToAll(message);
     }
 
     private void sendMessageToAll(TextMessage message) throws IOException {
         for (UserChatModel userModel : userList) {
-             userModel.sendMessage(message);
+             userModel.sendMessage(message.getPayload());
         }
     }
 
--- src/main/java/pl/oskarpolak/superchat/models/UserChatModel.java
+++ src/main/java/pl/oskarpolak/superchat/models/UserChatModel.java
@@ -2,7 +2,10 @@ package pl.oskarpolak.superchat.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
 
 @Data
 public class UserChatModel {
@@ -11,5 +14,9 @@ public class UserChatModel {
 
     public UserChatModel(WebSocketSession session){
         this.session = session;
+    }
+
+    public void sendMessage(String message) throws IOException {
+        session.sendMessage(new TextMessage(message));
     }
 }
