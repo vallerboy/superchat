6173330320f5698e7731bed8dea04a74d672dfc4
Oskar Polak <biuro@fivarto.pl>
2018-04-07 11:07:11 +0200

    Initial commit

---
+++ src/main/java/pl/oskarpolak/superchat/SuperchatApplication.java
@@ -0,0 +1,12 @@
+package pl.oskarpolak.superchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperchatApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/oskarpolak/superchat/models/ChatSocket.java
@@ -0,0 +1,38 @@
+package pl.oskarpolak.superchat.models;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@EnableWebSocket
+@Component
+public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry
+                .addHandler(this, "/chat")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Ktoś dołączył do chatu");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("Ktoś wyszedł z chatu");
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("Wiadomosc: " + message.getPayload());
+    }
+}
