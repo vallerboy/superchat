7bf1d1219cf64c3a7c82f8eafdbe0cbdd0d6d045
Oskar Polak <biuro@fivarto.pl>
2018-04-07 12:27:35 +0200

    kkk

---
+++ src/main/resources/static/chatScript.js
@@ -0,0 +1,21 @@
+    function onLoad() {
+        var ws = "ws://localhost:8080/chat"
+        websocket = new WebSocket(ws);
+        websocket.onopen = function (ev) {
+            onOpen(ev)
+        };
+        websocket.onmessage =  function (ev) {
+            onMessage(ev);
+        }
+    }
+
+    function onOpen(ev) {
+        console.log("Połączono");
+    }
+
+    function onMessage(ev) {
+        var message = ev.data;
+
+        messages.innerHTML = messages.innerHTML + "<li class = \"message\">" + message + "</li>";
+        messages.scrollTop = messages.scrollHeight;
+    }
